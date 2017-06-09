c5057f943bbd7b256ba6d3eee49044c78a9c2715
Abdus Samad Anwar <asa2107@gmail.com>
2017-06-09 12:35:29 -0500

    Working demo
    everything working

--- config/routes.rb
+++ config/routes.rb
@@ -38,7 +38,7 @@ Rails.application.routes.draw do
   # Routes for the Listing resource:
   # CREATE
   get "/listings/new", :controller => "listings", :action => "new"
-  post "/create_listing", :controller => "listings", :action => "create"
+  get "/create_listing", :controller => "listings", :action => "create"
 
   # READ
   get "/listings", :controller => "listings", :action => "index"
